faffd514f0f60cebae7591680309d7480dca5ec6
CJ <cj@iMac7.local>
2018-10-01 10:15:52 -0400

    made 4 crosses and colored them

--- assignment_three.py
+++ assignment_three.py
@@ -1,51 +1,69 @@
 import turtle
 
 
-
 def side_length():
     side = input("What is the length of the cross?")
     return float(side)
 
 
 def fill_color():
-     color = input("What color is the cross?")
-    turtle.begin_fill
-    return fill_color
+    color = input("What color is the cross?")
+    return color
 
 
 def draw_a_cross(side, color):
-    for x in range(2):
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.left(90)
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.left(90)
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.left(90)
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.right(90)
-        turtle.forward(side)
-        turtle.left(90)
-        turtle.forward(side)
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.right(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.end_fill()
+
 
 def move(x,y):
     turtle.up()
+    turtle.goto(x, y)
+    turtle.down
+
 
 def main():
     length = side_length()
-    draw_a_cross(length)
+    color = fill_color()
+    draw_a_cross(length, color)
+    turtle.up()
+    move(2 * length, -2 * length)
+    turtle.down
+    draw_a_cross(length, color)
+    move(0, 0)
+    turtle.backward(length)
+    draw_a_cross(length, color)
+    move(0, 0)
+    move(-length, -length * 2)
+    turtle.backward(length)
+    draw_a_cross(length, color)
+
 
     turtle.exitonclick()
+
 
 main()
